51541190a53245c60e919955dd9ad8691e4fb355
Deveesh <deveesh97@gmail.com>
2024-09-23 16:14:54 +0530

    Initial Commit

---
+++ SwiftUI TodoApp/SwiftUI_TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TodoAppApp.swift
+//  SwiftUI TodoApp
+//
+//  Created by Deveesh Mehandiratta on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
